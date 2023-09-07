5cc4dbb5d025b7a3a47a201f7c5cbc6e7a854496
dmitry_zaykin <orionvega5@list.ru>
2023-09-07 21:23:44 +0300

    Добавил анатацию типов

--- sem4_home1.py
+++ sem4_home1.py
@@ -1,13 +1,14 @@
 # Напишите функцию для транспонирования матрицы
 
 
-def print_matrix(my_list):
+def print_matrix(my_list: list[int|float]) -> None:
     """ Printing out two-dimensional matrix """
     for item in my_list:
         print(*item)
 
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
 print_matrix(matrix)
 
 trans_matrix = list(zip(*matrix))
