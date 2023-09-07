0c7ece4ffd8b08b0db3cb9b5ec917b330024c842
dmitry_zaykin <orionvega5@list.ru>
2023-09-07 21:08:12 +0300

    sem4_home1

---
+++ sem4_home1.py
@@ -0,0 +1,17 @@
+# Напишите функцию для транспонирования матрицы
+
+
+def print_matrix(my_list):
+    """ Printing out two-dimensional matrix """
+    for item in my_list:
+        print(*item)
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print_matrix(matrix)
+
+trans_matrix = list(zip(*matrix))
+
+print('\nТранспорированный результат: \n')
+
+print_matrix(matrix)
