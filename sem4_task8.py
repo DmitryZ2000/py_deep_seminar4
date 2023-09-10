2668c174e5951779328e849c1de6c1a0d6b21faf
dmitry_zaykin <orionvega5@list.ru>
2023-09-10 16:00:18 +0300

    Update the solution of sem4_task8

--- sem4_task8.py
+++ sem4_task8.py
@@ -5,12 +5,12 @@
 
 def change_dic(my_dict: dict) -> dict:
     """ Описание функции """
-    temp_dic = {}
+    temp_dict = {}
     for key, value in my_dict.items():        
-        if str(key)[-1] == 's' and  len(str(key)) > 1:
+        if key[-1] == 's' and  len(key) > 1:
             my_dict[key] = None
-            temp_dic[str(key)[0:-1]] = value 
-        my_dict = my_dict | temp_dic
+            temp_dict[key[0:-1]] = value 
+        my_dict = my_dict | temp_dict
     return my_dict
 
 seasons = 5
@@ -21,9 +21,8 @@ laptops = 200
 
 global_dict = {key: value for key, value in globals().items() if not key.startswith('__')}
 # global_dict_2 = dict(filter(lambda x: not x[0].startswith('__'),globals().items()))
-# print(global_dict_2)
+# print(global_dict2)
+print(globals())
 print(global_dict)
-
-
 print(change_dic(global_dict))
 
